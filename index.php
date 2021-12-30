8e00513f93b5af63c202b5cb7f9d6ee4980910dd
sunil007 <sunil.kothari.007@gmail.com>
2021-12-30 19:00:35 +0530

    Biyanis education site

---
+++ index.php
@@ -0,0 +1,298 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Biyani's Education Group</title>
+  <!-- web fonts -->
+  <link href="//fonts.googleapis.com/css?family=Roboto:400,700,900&display=swap" rel="stylesheet">
+  <link href="//fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+  <!-- web fonts -->
+
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="assets/css/style-starter.css?138">
+  <link rel="stylesheet" href="assets/css/font-icons.css">
+  <link rel="shortcut icon" href="./assets/images/fav.png">
+  <link rel="stylesheet" type="text/css" href="assets/css/timeline-style.css">
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-Y4CXFKX4L4"></script>
+  <script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+	  gtag('config', 'G-Y4CXFKX4L4');
+  </script>
+</head>
+<body>
+<?php include "include/nav.php"; ?>
+<section class="w3l-banner-slider">
+  <div id="carouselExampleCaptions" class="carousel slide carousel-fade" data-ride="carousel">
+    
+    <div class="carousel-inner">
+      <div class="carousel-item active" style="background-image: url('assets/images/education-banner1.jpg')">
+        <!-- <img src="assets/images/banner.jpg" class="d-block w-100" alt="..."> -->
+        <div class="carousel-caption container"> 
+		</div>
+      </div>
+      <div class="carousel-item" style="background-image: url('assets/images/education-banner2.jpg')">
+        <div class="carousel-caption container"> 
+		</div>
+      </div>
+      <div class="carousel-item" style="background-image: url('assets/images/education-banner3.jpg')">
+        <div class="carousel-caption container">
+        </div>
+      </div>
+    </div>
+	<ol class="carousel-indicators">
+      <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+    </ol>
+    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</section>
+<!-- index1 -->
+<div class="w3l-index1 py-4" id="about">
+  <div class="content-main pt-lg-5 pt-md-3 pb-0">
+    <div class="container">
+      <div class="row">
+		<div class="col-lg-12 text-center">	
+			<h3 style='font-size:2.3em;' class='bottomUnderline'>About Us</h3>
+		</div>
+		<div class="offset-lg-1 col-lg-2 text-center">
+			<img src='./assets/images/biyanis_symbol_lg -transparent-small.png' style='max-width: 200px;width: 100%;'>
+		</div>
+        <div class="col-lg-8">
+			<div style='height:20px'></div>
+			<p class="mb-0 big1Text">Another strong pillar of Biyani’s Education Group, we have established <b>Biyani’s JEE NEET Private</b> on the same principles that we are known for - complete transparency in education. Firmly rooted in the teaching pattern of Kota & the speed of Mumbai, we are a group of experienced directors who have teamed up for a single mission - to produce the best <span style="white-space: nowrap;font-weight: bold;color: #c52c4d ;text-decoration: underline;">JEE & NEET</span> results in the country.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- //index1 -->
+<section class="w3l-index2">
+  <div class="features-main pt-0">
+    <div class="container pb-lg-3">
+      <div class="row features text-center">
+		<div class="col-lg-6 col-sm-6 feature-grid">
+          <a target='_blank' href="https://www.biyanissuper30.com/">
+            <div class="feature-body shadowhover">
+              <div class="feature-img">
+                <img src='./assets/images/biyansi super 30.png' style='width:100%' />
+              </div>
+              <!--div class="feature-info mt-4">
+                <h3 class="feature-titel mb-3">Biyani's Super 30</h3>
+                </p>
+              </div-->
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-6 col-sm-6 feature-grid">
+          <a target='_blank' href="https://biyanisjeeneetprivate.com/">
+            <div class="feature-body shadowhover">
+              <div class="feature-img">
+                <img src='./assets/images/biyanis jee neet.jpg' style='width:100%' />
+              </div>
+              <!--div class="feature-info mt-4">
+                <h3 class="feature-titel mb-3">Biyani's JEE NEET Private</h3>
+              </div-->
+            </div>
+          </a>
+        </div> 
+      </div>
+	  <div class='row'>
+		<div class="col-lg-12 content-left mt-lg-0 mt-5 pl-lg-5">
+			<div class='text-center'>
+				<!--div data-toggle="modal" data-target="#enquireModal" class="theme-button btn-outline-primary btn mt-lg-5 mt-4">Enquire Now</div-->
+			</div>
+		</div>
+	  </div>
+    </div>
+  </div>
+  
+  <div class="w3l-index1 py-4 pb-5 cssanimations" id="about">
+	  <div class="content-main pt-lg-5 pt-md-3 pb-0">
+		<div class="container">
+			 <div class="row">
+				<div class="header-section col-lg-12">
+					<h3 class="bottomUnderline text-center">Our Journey</h3>
+				</div>
+				<div class="col-lg-12">
+					<div>&nbsp;</div>
+					<div class="row">
+						<div class="col-md-12">
+							<section id="cd-timeline-2" class="cd-container">
+								<div class="cd-timeline-block">
+									<div class="cd-timeline-img cd-picture">
+										<img src="./assets/images/camera-icon.png" alt="Picture">
+									</div>
+									<!--div class="cd-timeline-text">
+										<h2>Test Heading</h2>
+										<span>01 December 2000</span>
+									</div-->
+									<div class="cd-timeline-content projects">
+										<img src="./assets/images/banner.jpg" alt="">
+										<div class="project-content">
+											<h2>2014 <small>Started out Biyani’s Super 30 with the sole aim of changing the face of ICSE & CBSE education in town. 250+ Students Enrolled</small></h2>
+											<div style='height:10px;'></div>
+										</div>
+									</div>
+								</div>
+
+								<div class="cd-timeline-block">
+									<div class="cd-timeline-img cd-picture">
+										<img src="./assets/images/camera-icon.png" alt="Picture">
+									</div>
+
+									<div class="cd-timeline-content projects">
+										<img src="./assets/images/banner1.jpg" alt="">
+										<div class="project-content">
+											<h2>2015 <small>Opened a new branch in Mira Road and enrolled 450 students.
+ICSE & CBSE results soared high and more than 75% students scoring above 90%.</small></h2>
+											<div style='height:10px;'></div>
+										</div>
+									</div>
+								</div>
+
+								<div class="cd-timeline-block">
+									<div class="cd-timeline-img cd-picture">
+										<img src="./assets/images/camera-icon.png" alt="Picture">
+									</div>
+
+									<div class="cd-timeline-content projects">
+										<img src="./assets/images/banner2.jpg" alt="">
+										<div class="project-content">
+											<h2>2016 <small>We proved our mark in ICSE as our results reached new heights with a record breaking performance of scoring 95%.<br><b>600+ Students Enrolled</b></small></h2>
+											<div style='height:10px;'></div>
+										</div>
+									</div>
+								</div>
+
+								<div class="cd-timeline-block">
+									<div class="cd-timeline-img cd-picture">
+										<img src="./assets/images/camera-icon.png" alt="Picture">
+									</div>
+
+									<div class="cd-timeline-content projects">
+										<img src="./assets/images/banner3.jpg" alt="">
+										<div class="project-content">
+											<h2>2017 <small>Our first ICSE school topper - Srishti Gupta with 97%. More than 80% students scored above 90%.<br>Launched a new branch in Thakur Village, Kandivali (E).</small></h2>
+											<div style='height:10px;'></div>
+										</div>
+									</div>
+								</div>
+
+								<div class="cd-timeline-block">
+									<div class="cd-timeline-img cd-picture">
+										<img src="./assets/images/camera-icon.png" alt="Picture">
+									</div>
+
+									<div class="cd-timeline-content projects">
+										<img src="./assets/images/banner4.jpg" alt="">
+										<div class="project-content">
+											<h2>
+												2018 
+												<small>
+													Repeating history with the top results in town - 97.6% by Shalini in ICSE 2018. 7 out of 10 school toppers of RBK were Biyani’s Super 30 students.<br>
+													Won the <b>‘Best Coaching Institute in Mumbai’</b> at RadioCity Annual Awards.<br>
+													Inaugurating a new branch in Shrishti Complex, Mira Road.<br>
+													1200+ ICSE & CBSE Students Enrolled with toppers from 4 major schools of Mumbai
+												</small>
+											</h2>
+											<div style='height:10px;'></div>
+											<span>01 December 2000</span>
+										</div>
+									</div>
+								</div>
+								
+								<div class="cd-timeline-block">
+									<div class="cd-timeline-img cd-picture">
+										<img src="./assets/images/camera-icon.png" alt="Picture">
+									</div>
+
+									<div class="cd-timeline-content projects">
+										<img src="./assets/images/banner3.jpg" alt="">
+										<div class="project-content">
+											<h2>
+												2019 
+												<small>
+													Year of School Toppers - RBK, TPS, Seven Square Academy, RBK Global, R.N. Podar, and Ryan International!<br>
+													Record-breaking performance with Prakhar Mundra & Asavari scoring 98.20 in ICSE 2019 and Arnav Jhanwar scoring 98.40% in CBSE 2019.<br>
+													Yash Biyani, Director of Biyani’s Education Group, was featured in The Business Standard India.
+												</small>
+											</h2>
+											<div style='height:10px;'></div>
+										</div>
+									</div>
+								</div>
+								
+								<div class="cd-timeline-block">
+									<div class="cd-timeline-img cd-picture">
+										<img src="./assets/images/camera-icon.png" alt="Picture">
+									</div>
+
+									<div class="cd-timeline-content projects">
+										<img src="./assets/images/banner4.jpg" alt="">
+										<div class="project-content">
+											<h2>
+												2020 
+												<small>
+													Achieved Mumbai City Rank 3 in ICSE 2020 with a stellar score of 98.8% by Aditi Agarwal and 98.2% by Preksha Mishra.<br>
+													Opened another centre in Mahavir Nagar, Kandivali (W).<br>
+													Launched Prayaas Test Series. Created online learning modules with B-Right Learning<br>
+													More than 1600 students enrolled
+												</small>
+											</h2>
+											<div style='height:10px;'></div>
+											<span>01 December 2000</span>
+										</div>
+									</div>
+								</div>
+								
+								<div class="cd-timeline-block">
+									<div class="cd-timeline-img cd-picture">
+										<img src="./assets/images/camera-icon.png" alt="Picture">
+									</div>
+
+									<div class="cd-timeline-content projects">
+										<img src="./assets/images/banner3.jpg" alt="">
+										<div class="project-content">
+											<h2>
+												2021 
+												<small>
+													Another golden feather in our hat as we produced Rank 3 in Mumbai - Shreya Yadav (99.2%, ICSE 2021) and school toppers of RBK G, TPS, RBK and Seven Square Academy.<br>
+													Rebranded to Biyani’s Education Group and launched Biyani’s JEE NEET Private focused on JEE, NEET & Foundation Courses.<br>
+													Opened two new centres in Goregaon and Thane
+												</small>
+											</h2>
+											<div style='height:10px;'></div>
+										</div>
+									</div>
+								</div>
+								
+							</section>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	  </div>
+	</div>
+</section>
+<?php //include 'include/footerEnquery.php'; ?>
+<?php include 'include/footer.php'; ?>
+<?php include 'include/js.php'; ?>
+<?php include 'include/enquireModal.php'; ?>
+<script type="text/javascript" src="assets/js/custom.js"></script>
+</body>
+
+</html>
